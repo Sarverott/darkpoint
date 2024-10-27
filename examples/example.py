644041589a2103aa1d79a0106a343eef7f2884e7
Sett Sarverott <sarverott@gmail.com>
2024-10-27 05:46:13 +0100

    update of examples... this single file is messy

--- examples/example.py
+++ examples/example.py
@@ -16,7 +16,11 @@
     # You should have received a copy of the GNU General Public License
     # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from ..src.darkpoint.Point import Point
+from darkpoint.Point import Point 
+# WARNING: needs package instalation
+# SEE:
+#   - dev/install-local.sh
+#   - dev/install-public.sh
 
 # definition of palace
 
