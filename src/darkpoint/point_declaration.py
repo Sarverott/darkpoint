85dc87028bd3ffae4370183487898cc62f51f06f
Sett Sarverott <sarverott@gmail.com>
2024-10-27 14:42:10 +0100

    still fighting with linter

--- src/darkpoint/point_declaration.py
+++ src/darkpoint/point_declaration.py
@@ -61,18 +61,16 @@ class Point:
             self.hook_name_buffer = hook_name  # setting hook name
         else:
             raise ValueError("non-Point adding is forbidden!")
-        
+
         return self
 
     def __add__(self, point_to_hook):
         if not isinstance(point_to_hook, Point):
             raise ValueError("non-Point adding is forbidden!")
-        
+
         self.hooks[self.hook_name_buffer] = point_to_hook  # setting point on hook
         self.hook_name_buffer = self.context  # context as default hook_name_buffer
         return point_to_hook  # Return hooked point for chaining
-        
-            
 
     def __iter__(self):
         return iter(self.hooks.items())
@@ -87,16 +85,17 @@ class Point:
         # print("\t", self)
         # print(f"\t?-{self.context} == {old_context}")
         if self.context == old_context:
-            tmp = None
+            # tmp = None
             # print(f"\t\tchange context from {old_context} to {new_context}")
-            for hook_name, hooked_point in (self / new_context):
+            for hook_name, hooked_point in self / new_context:
+                hooked_point.change_context_recursively(new_context, old_context)
                 # print(f"\t\t...asking point hooked on {hook_name}")
-                if hooked_point.change_context_recursively(new_context, old_context):
-                    tmp = hook_name
-            return True
-        else:
-            # print("\t\tno context change")
-            return False
+                # if hooked_point.change_context_recursively(new_context, old_context):
+                # tmp = hook_name
+            # return True
+        # else:
+        # print("\t\tno context change")
+        # return False
 
     def __floordiv__(self, new_context):
         if self.context != new_context:  # infinite loop check
