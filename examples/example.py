f3085e713c7d5aff8bf2e69699a1946866c309ba
Sett Sarverott <sarverott@gmail.com>
2024-10-27 05:05:09 +0100

    fixing path error of import in 'example.py'

--- examples/example.py
+++ examples/example.py
@@ -16,7 +16,7 @@
     # You should have received a copy of the GNU General Public License
     # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from src.darkpoint.Point import Point
+from ..src.darkpoint.Point import Point
 
 # definition of palace
 
